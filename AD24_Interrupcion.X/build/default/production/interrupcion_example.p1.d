d4b6fd918688414f0c7d5c66ca54aa98e7c9c8c7
Atoany <73314754+AtoanyTec@users.noreply.github.com>
2024-10-17 17:01:49 -0600

    Add files via upload

---
+++ AD24_Interrupcion.X/build/default/production/interrupcion_example.p1.d
@@ -0,0 +1,4 @@
+ build/default/production/interrupcion_example.d  \
+ build/default/production/interrupcion_example.p1:  \
+ interrupcion_example.c  \
+fusibles.h 
---
+++ AD24_Interrupcion.X/fusibles2.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   fusibles2.h
+ * Author: afier
+ *
+ * Created on October 14, 2024, 5:15 PM
+ */
+
+#ifndef FUSIBLES2_H
+#define	FUSIBLES2_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* FUSIBLES2_H */
+
